5c4fa15a5bf870f394ac6fc6280942fab35c22b0
Laura Daniela Guevara <laura.guevara02@uptc.edu.co>
2024-09-24 15:53:50 -0500

    primary scene style

--- src/main/java/co/edu/uptc/linkedlistworkshop/view/Runner.java
+++ src/main/java/co/edu/uptc/linkedlistworkshop/view/Runner.java
@@ -4,6 +4,9 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -14,16 +17,44 @@ public class Runner extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         VBox root = new VBox();
+        GridPane grid = new GridPane();
         Scene scene = new Scene(root, 800, 800);
-        scene.getStylesheets().add(new File("src/main/java/co/edu/uptc/linkedlistworkshop/View/Style.css").toURI().toString());
+        scene.getStylesheets().add(new File("src/main/java/co/edu/uptc/linkedlistworkshop/view/Style.css").toURI().toString());
 
-        Button createLabel = new Button("Create a new moto");
-        Button readLabel = new Button("View all motos");
-        Button updateLabel = new Button("Update moto information");
-        Button deleteLabel = new Button("Delete moto");
+        Label title = new Label("Motorcycle information management");
+        Label subtitle = new Label("Select an option");
 
-        root.getChildren().addAll(createLabel, readLabel, updateLabel, deleteLabel);
+        Label createLabel = new Label("1. Create a new moto");
+        Label readLabel = new Label("2. View all motos");
+        Label updateLabel = new Label("3. Update moto information");
+        Label deleteLabel = new Label("4. Delete moto");
+
+        Button createBut = new Button("Create");
+        Button readBut = new Button("Read");
+        Button updateBut = new Button("Update");
+        Button deleteBut = new Button("Delete");
+
+        // Column, row
+        grid.add(createLabel, 0, 0);
+        grid.add(readLabel, 0, 1);
+        grid.add(updateLabel, 0, 2);
+        grid.add(deleteLabel, 0, 3);
+        grid.add(createBut, 1, 0);
+        grid.add(readBut, 1, 1);
+        grid.add(updateBut, 1, 2);
+        grid.add(deleteBut, 1, 3);
+
+        title.setId("title");
+        root.setId("root");
+
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(50);
+        grid.setVgap(50);
+
+        root.getChildren().addAll(title, subtitle, grid);
         root.setAlignment(Pos.CENTER);
+        root.setSpacing(50);
+
         stage.setTitle("Motos");
         stage.setScene(scene);
         stage.show();
